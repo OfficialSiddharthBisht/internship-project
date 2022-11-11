fa1ea8202864243eab275a5676bd6c2441c13ec0
Siddharth <siddharth@flamecloud.co.uk>
2022-11-11 12:46:46 +0530

    ADD: react-bootstrap configuration

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 
 const root = document.getElementById("root");
 ReactDOM.render(
