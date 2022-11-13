88c5335c1fe01f3b1731dc0b232040054ddd18fc
Siddharth <siddharth@flamecloud.co.uk>
2022-11-13 19:50:45 +0530

    ADD:Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
 function Login() {
   return (
     <>
-      
+      <Card className="text-center" style={{ width: "50%", margin: "auto", backgroundColor: "#404040", marginTop: "140px", minHeight: "400px",background: "linear-gradient(92.01deg, #42A04B 0%, #06AC70 100%)" }}>
+        <Card.Body>
+          <Card.Text>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <span>
+                <img src="./static/icons/logo.png" alt="" width={"40px"} height={"40px"} />
+                <span style={{ fontSize: "40px", color: "#FFFFFF" }}>Account</span>
+              </span>
+              <span><h6 style={{color: "#FF7A49"}}>Sign Up</h6></span>
+            </div>
+            <br />
+            <Col>
+              <InputGroup size="sm" className="mb-3">
+                <img src="./static/icons/email.png" alt="" width={"55px"} height={"55px"} style={{ margin: "8px" }} />
+                <FloatingLabel
+                  controlId="floatingInput"
+                  label="Email"
+                  className="mb-3"
+                >
+                  <Form.Control aria-label="Small"
+                    aria-describedby="inputGroup-sizing-sm"
+                    type="text" placeholder="Email" />
+                </FloatingLabel>
+              </InputGroup>
+            </Col>
+            <Col>
+              <InputGroup size="sm" className="mb-3">
+                <img src="./static/icons/password.png" alt="" width={"55px"} height={"55px"} style={{ margin: "8px" }} />
+                <FloatingLabel
+                  controlId="floatingInput"
+                  label="Password"
+                  className="mb-3"
+                >
+                  <Form.Control aria-label="Small"
+                    aria-describedby="inputGroup-sizing-sm"
+                    type="password" placeholder="Password" />
+                </FloatingLabel>
+              </InputGroup>
+            </Col>
+          </Card.Text>
+          <Button variant="primary">Sign In</Button>
+          <br />
+          <small>Or sign in with</small>
+          <Container>
+            <Row>
+              <Col>
+                <img src="./static/icons/telegram3.png" alt="" />
+              </Col>
+              <Col>
+                <img src="./static/icons/google.png" alt="" />
+              </Col>
+            </Row>
+          </Container>
+        </Card.Body>
+      </Card>
     </>
   )
 }
