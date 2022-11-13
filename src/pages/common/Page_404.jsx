2a1959e87dba2986ace8b1c69242b0eb3b4a3e20
Siddharth <siddharth@flamecloud.co.uk>
2022-11-13 19:15:48 +0530

    404 Page complete

--- src/pages/common/Page_404.jsx
+++ src/pages/common/Page_404.jsx
@@ -2,26 +2,24 @@ import React from 'react'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 function _404() {
   return (
-    <div style={{ backgroundColor: "#414141", color: "#FFFFFF", height: "100vh" }}>
-      <Row className="justify-content-md-center">
-        <Col md="auto">
-          <Row>
-            <Col>
-              <img src="./static/icons/logo.png" alt="" width={"50px"} height={"50px"} />
-              <h3>LABELFALCON </h3>
-            </Col>
-          </Row>
-          <br />
-          <img src="./static/icons/404.png" alt="404" />
-          <h3>Oops... Page Not Found</h3>
-          <p>Maybe we have broken this page.<br />Come back to the homepage</p>
-          <Button style={{ background: "#E65425" }}>← Back To Homepage</Button>{' '}
-        </Col>
-      </Row>
-    </div>
+    <Card className="text-center" style={{border:"none",height:"100vh"}}>
+      <Card.Body style={{backgroundColor:"#0F0F0F"}}>
+        <Card.Text>
+          <img src="./static/icons/logo.png" alt="" width={"40px"} height={"40px"} />
+          <span style={{ fontSize: "40px" }}>LABELFALCON</span>
+          <div>
+            <img src="./static/icons/404.png" alt="404" />
+            <h3>Oops... Page Not Found</h3>
+            <br />
+            <p>Maybe we have broken this page.<br />Come back to the homepage</p>
+          </div>
+          <Button style={{color:"#FFFFFF",fontWeight:"bold"}}>← Back To Homepage</Button>{' '}
+        </Card.Text>
+      </Card.Body>
+    </Card>
   )
 }
-
 export default _404;
