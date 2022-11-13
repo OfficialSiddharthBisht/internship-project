5c05058264ed303c1bb5786aba580ed42f2dc9c2
Siddharth <siddharth@flamecloud.co.uk>
2022-11-13 11:01:19 +0530

    CHANGE: primary color using saas

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import './custom.scss';
 
 const root = document.getElementById("root");
 ReactDOM.render(
