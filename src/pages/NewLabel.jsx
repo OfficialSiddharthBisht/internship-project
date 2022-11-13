53039e488f2b966d8b47734b2f2239a18ada45f0
Siddharth <siddharth@flamecloud.co.uk>
2022-11-13 10:33:38 +0530

    New Label Component Complete

--- src/pages/NewLabel.jsx
+++ src/pages/NewLabel.jsx
@@ -11,6 +11,24 @@ function NewLabel() {
     const [senderReceiverInfo, setSenderReceiverInfo] = useState([
         "Name", "Phone", "Address line 1", "Address line 2", "Address line 3", "City", "ZIP", "State"
     ])
+    const [generalDataMetric, setGeneralDataMetric] = useState([
+        {
+            metric: "Weight",
+            placeholder: "0.00 lbs",
+        },
+        {
+            metric: "Length",
+            placeholder: "0.00 inch",
+        },
+        {
+            metric: "Width",
+            placeholder: "0.00 inch",
+        },
+        {
+            metric: "Height",
+            placeholder: "0.00 inch",
+        },
+    ])
     return (
         <>
             <Container>
@@ -59,10 +77,56 @@ function NewLabel() {
                             </Card.Body>
                         </Card>
                     </Col>
-                    <Col>3 of 3</Col>
+                    <Col>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>③General Data</Card.Title>
+                                <Card.Text>
+                                    <Col>
+                                        <Form.Select aria-label="Default select example">
+                                            <option>Provider</option>
+                                            <option value="1">UPS</option>
+                                            <option value="2">APS</option>
+                                            <option value="3">TPS</option>
+                                        </Form.Select>
+                                    </Col>
+                                    <br />
+                                    <Col>
+                                        <Form.Select aria-label="Default select example">
+                                            <option>Shipping Speed</option>
+                                            <option value="1">Immediate</option>
+                                            <option value="2">Can Wait</option>
+                                            <option value="3">Economic</option>
+                                        </Form.Select>
+                                    </Col>
+                                    <br />
+                                    {
+                                        generalDataMetric.map((el, index) => {
+                                            return (
+                                                <Col>
+                                                    <Form.Label htmlFor="basic-url">{el.metric}</Form.Label>
+                                                    <InputGroup size="sm" className="mb-3">
+                                                        <Form.Control aria-label="Small"
+                                                            aria-describedby="inputGroup-sizing-sm"
+                                                            type="number" placeholder={el.placeholder} />
+                                                    </InputGroup>
+                                                </Col>
+                                            )
+                                        })
+                                    }
+                                </Card.Text>
+                                <Button variant="light">
+                                    <img src="./static/icons/price.png" alt="" />
+                                    Price: <span style={{color:"#E65425" }}>$0.00</span>
+                                </Button>
+                                <small>Questions? FAQ</small>
+                            </Card.Body>
+                        </Card>
+                    </Col>
                 </Row>
+                <br />
                 <Row>
-                    Create New Label
+                    <Button variant="light" style={{ border: "1px dashed #E65425",color:"#E65425" }}>+ Create New Label</Button>{' '}
                 </Row>
             </Container>
         </>
