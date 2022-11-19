d7edc1b77cb7d9e8598f4b7736bf6a80a5cb9204
Siddharth <siddharth@flamecloud.co.uk>
2022-11-19 15:51:16 +0530

    Nav searchbar improved

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -7,16 +7,18 @@ function Header() {
     const [modalShow, setModalShow] = useState(false);
     return (
         <>
-            <Form.Group size="sm" className="mb-3 navbar-search" style={{ display: "flex", marginTop: "0px", width: "98%", margin: "auto", border: "2px solid grey", borderRadius: "10px", marginTop: "-34px" }}>
-                <Form.Control type="text" className='border-0' placeholder="Search by ID, To Name, From Name.." />
-                <img src="./static/icons/notifybtn.png" alt="" style={{ margin: "0px 5px 0px 5px" }} />
-                <img src="./static/icons/logout.png" alt=""
+            <div style={{ display: "flex", marginTop: "0px", width: "100%", margin: "auto", marginTop: "-34px" }}>
+                <Form.Group size="sm" className="mb-3 navbar-search" style={{ minWidth: "90%" }}>
+                    <Form.Control type="text" className='border-1' placeholder="Search by ID, To Name, From Name.." />
+                </Form.Group>
+                <img src="./static/icons/notifybtn.png" alt="" height={"40px"} style={{ margin: "0px 5px 0px 5px" }} />
+                <img src="./static/icons/logout.png" alt="" height={"40px"}
                     style={{ margin: "0px 5px 0px 5px" }}
                     onClick={() => {
                         setModalShow(true);
                     }}
                 />
-            </Form.Group>
+            </div>
             <LogoutPopup
                 show={modalShow}
                 onHide={() => setModalShow(false)}
