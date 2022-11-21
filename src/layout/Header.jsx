323af35d4e84bb863a4b9ea2bb17f05413df7918
Siddharth <siddharth@flamecloud.co.uk>
2022-11-21 12:21:10 +0530

    Display none of navbar at mobile version

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Form from 'react-bootstrap/Form';
 import LogoutPopup from './LogoutPopup';
 function Header() {
+    const [isMobile, setisMobile] = useState(false);
     const [modalShow, setModalShow] = useState(false);
+    useEffect(() => {
+        window.addEventListener(
+            "resize",
+            () => {
+                setisMobile(window.innerWidth < 1200);
+            },
+            false
+        );
+    }, []);
+
+    useEffect(() => {
+        setisMobile(window.innerWidth < 1200);
+    });
     return (
         <>
-            <div style={{ display: "flex", marginTop: "0px", width: "100%", margin: "auto", marginTop: "-34px" }}>
-                <Form.Group size="sm" className="mb-3 navbar-search" style={{ minWidth: "80%" }}>
+            <div
+                className={isMobile ? "d-none" : "d-flex mb-3 navbar-search"}
+                style={{ marginTop: "0px", width: "100%", margin: "auto", marginTop: "-34px" }}>
+                <Form.Group size="sm" style={{ minWidth: "90%" }}>
                     <Form.Control type="text" className='border-1' placeholder="Search by ID, To Name, From Name.." />
                 </Form.Group>
                 <img src="./static/icons/notifybtn.png" alt="" height={"40px"} style={{ margin: "0px 5px 0px 5px" }} />
@@ -24,6 +40,5 @@ function Header() {
         </>
     );
 }
-
 
 export default Header
